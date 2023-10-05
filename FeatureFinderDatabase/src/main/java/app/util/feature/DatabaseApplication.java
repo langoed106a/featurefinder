37da1463e8e3415585f361b4edee286e164b575c
vboxuser <vboxuser@localhost.localdomain>
2023-10-05 20:19:40 +0100

    added

--- FeatureFinderDatabase/src/main/java/app/util/feature/DatabaseApplication.java
+++ FeatureFinderDatabase/src/main/java/app/util/feature/DatabaseApplication.java
@@ -4,15 +4,18 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"app.util.feature"})
+@ComponentScan(basePackages = {"app.util.feature","app.util.database"})
 public class DatabaseApplication implements CommandLineRunner {
-	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
     public static void main(String[] args) {
         SpringApplication.run(DatabaseApplication.class, args);
     }
@@ -20,11 +23,8 @@ public class DatabaseApplication implements CommandLineRunner {
     @Override
 	public void run(String... args) throws Exception {
 		//Create the database table:
-		//jdbcTemplate.execute("CREATE TABLE IF NOT EXISTS featuredocumentstore(id INTEGER PRIMARY KEY,name TEXT,type TEXT,description TEXT,contents BLOB)");
+		jdbcTemplate.execute("CREATE TABLE IF NOT EXISTS featuredocumentstore(id INTEGER PRIMARY KEY,name TEXT,type TEXT,description TEXT,contents BLOB)");
 		
-		//Insert a record:
-		//jdbcTemplate.execute("INSERT INTO beers VALUES ('Stella')");
-
 		//Read records:
 		// List<FeatureDocument> documents = jdbcTemplate.query("SELECT * FROM featuredocumentstore",(resultSet, rowNum) -> new FeatureDocument(resultSet.getInt("id"),resultSet.getString("name"),resultSet.getString("type"),resultSet.getString("description"),resultSet.getString("contents")));
 		
