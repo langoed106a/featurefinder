ef9976bd237c2ce1749109108b5d917c87364af4
vboxuser <vboxuser@localhost.localdomain>
2023-11-20 20:46:00 +0000

    added

--- FeatureFinderDatabase/src/main/java/app/util/database/DocumentDatabase.java
+++ FeatureFinderDatabase/src/main/java/app/util/database/DocumentDatabase.java
@@ -20,7 +20,7 @@ public class DocumentDatabase {
     }
 
     public Document getDocument(Integer id) {
-        List<Document> documents = jdbcTemplate.query("SELECT * FROM featuredocumentstore WHERE id="+id,(resultSet, rowNum) -> new Document(resultSet.getInt("id"),resultSet.getString("name"),resultSet.getString("type"),resultSet.getBinaryStream("contents"),resultSet.getString("description")));
+        List<Document> documents = jdbcTemplate.query("SELECT * FROM featuredocumentstore WHERE id="+id,(resultSet, rowNum) -> new Document(resultSet.getInt("id"),resultSet.getString("name"),resultSet.getString("type"),resultSet.getBinaryStream("contents"),resultSet.getString("description"),resultSet.getString("label"),resultSet.getString("origin")));
         if ((documents!=null) && (documents.size()>=0)) {
             return documents.get(0);
         } else {
@@ -29,7 +29,7 @@ public class DocumentDatabase {
     }
 
     public Document getDocumentById(Integer id) {
-        List<Document> documents = jdbcTemplate.query("SELECT * FROM featuredocumentstore WHERE id="+id,(resultSet, rowNum)-> new Document(resultSet.getInt("id"),resultSet.getString("name"),resultSet.getString("type"),resultSet.getBinaryStream("contents"),resultSet.getString("description")));
+        List<Document> documents = jdbcTemplate.query("SELECT * FROM featuredocumentstore WHERE id="+id,(resultSet, rowNum)-> new Document(resultSet.getInt("id"),resultSet.getString("name"),resultSet.getString("type"),resultSet.getBinaryStream("contents"),resultSet.getString("description"),resultSet.getString("label"),resultSet.getString("origin")));
         if ((documents!=null) && (documents.size()>=0)) {
             return documents.get(0);
         } else {
@@ -38,7 +38,7 @@ public class DocumentDatabase {
     }
 
     public Document getDocumentByName(String name) {
-        List<Document> documents = jdbcTemplate.query("SELECT * FROM featuredocumentstore WHERE name=\""+name+"\"",(resultSet, rowNum)-> new Document(resultSet.getInt("id"),resultSet.getString("name"),resultSet.getString("type"),resultSet.getBinaryStream("contents"),resultSet.getString("description")));
+        List<Document> documents = jdbcTemplate.query("SELECT * FROM featuredocumentstore WHERE name=\""+name+"\"",(resultSet, rowNum)-> new Document(resultSet.getInt("id"),resultSet.getString("name"),resultSet.getString("type"),resultSet.getBinaryStream("contents"),resultSet.getString("description"),resultSet.getString("label"),resultSet.getString("origin")));
         if ((documents!=null) && (documents.size()>=0)) {
             return documents.get(0);
         } else {
@@ -53,7 +53,7 @@ public class DocumentDatabase {
     }
 
     public String updateDocument(Document document) {
-        String query = "UPDATE featuredocumentstore SET name=?,type=?,contents=?,description=? WHERE id=?";
+        String query = "UPDATE featuredocumentstore SET name=?,type=?,contents=?,description=?,label=?,origin=? WHERE id=?";
         Boolean result = jdbcTemplate.execute(query, new FeatureDocumentPreparedStatement(document));
         String reply = "Document has been updated";
         return reply;
@@ -68,7 +68,7 @@ public class DocumentDatabase {
     }
 
     public List<Document> getDocumentByGroup(String groupname) {
-        List<Document> documents = jdbcTemplate.query("SELECT * FROM featuredocumentstore WHERE type=\""+groupname+"\"",(resultSet, rowNum) -> new Document(resultSet.getInt("id"),resultSet.getString("name"),resultSet.getString("type"),resultSet.getBinaryStream("contents"),resultSet.getString("description")));
+        List<Document> documents = jdbcTemplate.query("SELECT * FROM featuredocumentstore WHERE type=\""+groupname+"\"",(resultSet, rowNum) -> new Document(resultSet.getInt("id"),resultSet.getString("name"),resultSet.getString("type"),resultSet.getBinaryStream("contents"),resultSet.getString("description"),resultSet.getString("label"),resultSet.getString("origin")));
         if ((documents!=null) && (documents.size()>=0)) {
             return documents;
         } else {
@@ -77,7 +77,7 @@ public class DocumentDatabase {
     }
 
     public List<Document> getDocumentByType(String type) {
-        List<Document> documents = jdbcTemplate.query("SELECT * FROM featuredocumentstore WHERE type=\""+type+"\"",(resultSet, rowNum) -> new Document(resultSet.getInt("id"),resultSet.getString("name"),resultSet.getString("type"),resultSet.getBinaryStream("contents"),resultSet.getString("description")));
+        List<Document> documents = jdbcTemplate.query("SELECT * FROM featuredocumentstore WHERE type=\""+type+"\"",(resultSet, rowNum) -> new Document(resultSet.getInt("id"),resultSet.getString("name"),resultSet.getString("type"),resultSet.getBinaryStream("contents"),resultSet.getString("description"),resultSet.getString("label"),resultSet.getString("origin")));
         if ((documents!=null) && (documents.size()>=0)) {
             return documents;
         } else {
