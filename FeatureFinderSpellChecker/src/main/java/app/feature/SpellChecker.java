960c357803aba1e3b9b6481d9e2ab253603e2873
vboxuser <vboxuser@localhost.localdomain>
2024-01-28 09:58:31 +0000

    added

--- FeatureFinderSpellChecker/src/main/java/app/feature/SpellChecker.java
+++ FeatureFinderSpellChecker/src/main/java/app/feature/SpellChecker.java
@@ -7,18 +7,18 @@ import org.languagetool.Languages;
 import org.languagetool.rules.RuleMatch;
 
 public class SpellChecker {
+    private JLanguageTool languageTool;
 
     public SpellChecker() {
-
+        languageTool = new JLanguageTool(Languages.getLanguageForShortCode("en-GB"));
     }
 
     public String checkSpelling(String text) {
        String reply="";
-       JLanguageTool langTool = new JLanguageTool(Languages.getLanguageForShortCode("en-GB"));
     // comment in to use statistical ngram data:
     //langTool.activateLanguageModelRules(new File("/data/google-ngram-data"));
        try {
-       List<RuleMatch> matches = langTool.check(text);
+       List<RuleMatch> matches = languageTool.check(text);
        for (RuleMatch match : matches) {
           System.out.println("Potential error at characters " +
           match.getFromPos() + "-" + match.getToPos() + ": " +
